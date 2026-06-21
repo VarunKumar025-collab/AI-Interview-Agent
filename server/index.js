93cef698f3becdadcc98c28e346f1f1803eaf8c2
VarunKumar025-collab <varungangwar025@gmail.com>
2026-06-21 12:56:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-interview-agent-1-client-7n4z.onrender.com",
     credentials:true
 }))
 
